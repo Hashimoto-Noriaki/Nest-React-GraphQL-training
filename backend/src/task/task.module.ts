13180fe688e36bad01d25132bc6ca77e4e8b3e71
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-06 01:55:16 +0900

    taskmoduleにimportを追記

--- backend/src/task/task.module.ts
+++ backend/src/task/task.module.ts
@@ -3,6 +3,7 @@ import { TaskResolver } from './task.resolver';
 import { TaskService } from './task.service';
 
 @Module({
+  imports: [PrismaModule],
   providers: [TaskResolver, TaskService],
 })
 export class TaskModule {}

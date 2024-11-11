e7d91e386ddd9622abef223815825b1bd445e832
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-12 03:35:02 +0900

    タスク更新機能

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Query, Mutation, Resolver } from '@nestjs/graphql';
 import { Task as TaskModel } from './models/task.model';
 import { TaskService } from './task.service';
 import { CreateTaskInput } from './dto/CreateTask.input';
+import { UpdateTaskInput } from './dto/UpdateTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -17,5 +18,12 @@ export class TaskResolver {
     @Args('createTaskInput') createTaskInput: CreateTaskInput,
   ): Promise<TaskModel> {
     return await this.taskService.createTask(createTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async updateTask(
+    @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
+  ): Promise<TaskModel> {
+    return await this.taskService.updateTask(updateTaskInput);
   }
 }

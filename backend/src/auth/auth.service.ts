0a2af99d086cae04cbde3d5459a2f41f4fd0533b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-16 01:01:25 +0900

    バリデーションユーザーメソッドの実装

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userService.getUser(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+}

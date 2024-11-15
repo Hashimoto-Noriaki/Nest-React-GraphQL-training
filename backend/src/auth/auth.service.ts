4a29e228607f9428477c099b0b9a0aa4e8181477
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-16 04:33:59 +0900

    サインインメソッド実装

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { SignInResponse } from './dto/signinResponse';
+import { JwtPayload } from './types/jwtPayload.type';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +20,10 @@ export class AuthService {
       return user;
     }
     return null;
+  }
+
+  async signIn(user: User): Promise<SignInResponse> {
+    const payload: JwtPayload = { email: user.email, sub: user.id };
+    return { accessToken: this.jwtService.sign(payload), user };
   }
 }

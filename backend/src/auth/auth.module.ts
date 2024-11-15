f97717cbdf7122867dd1a4187b584c3d962644a5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-15 22:31:29 +0900

    35. サインイン機能の実装【ライブラリのインストールとAuthモジュールの設定】

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { UserModel } from 'src/user/models/user.model';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthResolver, AuthService]
+  imports: [
+    UserModel,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}

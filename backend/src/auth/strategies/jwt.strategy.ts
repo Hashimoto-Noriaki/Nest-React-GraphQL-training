e1c463c002ca4024f2832d945c6539bf567194af
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-11-17 13:05:42 +0900

    jwt Strategyのバリデーション

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserService } from 'src/user/user.service';
+import { JwtPayload } from '../types/jwtPayload.type';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,5 +13,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
+  }
+
+  async validate(payload: JwtPayload): Promise<User | null> {
+    return await this.userService.getUser(payload.email);
   }
 }

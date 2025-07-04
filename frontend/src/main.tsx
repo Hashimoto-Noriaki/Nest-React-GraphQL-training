026499f819e76277eb265ddd8135f3a00ec35201
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-04 22:43:36 +0900

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import 'modern-css-reset';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )

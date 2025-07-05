bb7b4a348f4d8d1246eb9005a583f2fe4e16a8fe
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-06 04:29:12 +0900

    ApolloClientのセットアップ完了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,23 @@ import SignUp from './components/pages/SignUp'
 import Main from './components/pages/Main'
 import NotFound from './components/pages/NotFound'
 import { GuestRoute,PrivateRoute } from './AuthRoute'
+import client from './apollo/client'
+import { ApolloProvider } from '@apollo/client'
 import './App.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PrivateRoute children={<Main/>}/>}/>
-        <Route path='/signin' element={<GuestRoute children={<SignIn/>}/>}/>
-        <Route path='/signup' element={<GuestRoute children={<SignUp/>}/>}/>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PrivateRoute children={<Main/>}/>}/>
+          <Route path='/signin' element={<GuestRoute children={<SignIn/>}/>}/>
+          <Route path='/signup' element={<GuestRoute children={<SignUp/>}/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 

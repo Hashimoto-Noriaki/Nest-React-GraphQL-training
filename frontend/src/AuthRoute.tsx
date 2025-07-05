ad900d0f442a1bada127220a0bab6ef1de76d738
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-06 03:48:29 +0900

    ゲストページ用のルート

--- frontend/src/AuthRoute.tsx
+++ frontend/src/AuthRoute.tsx
@@ -1,3 +1,4 @@
+//プライベートページ用のルート
 import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './hooks/useAuth';
@@ -16,5 +17,19 @@ export const PrivateRoute = ({children}:Props) => {
         return <>{children}</>; // 認証済みの場合、子コンポーネントを表示 true
     }
 
-    return <Navigate to='/signin' />
+    return <Navigate to='/signin' /> // 認証されていない場合、サインインページへリダイレクト
+}
+
+//ゲストページ用のルート
+export const GuestRoute = ({children}:Props) => {
+    const authInfo = useAuth();//カスタムフックのuseAuthを呼び出す
+    if(!authInfo.checked){
+        return <div>Loading...</div>; // 認証チェック中の表示
+    }
+
+    if(autuInfo.authenticated){
+        return <Navigate to='/'/>; // 認証済みの場合、子コンポーネントを表示 true
+    }
+
+    return <>{children}</>; // 認証されていない場合、子コンポーネントを表示
 }
